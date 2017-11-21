af4b9f38b12f0678bda9beef0f987cce9e7b67fa
riasavari <ria.savari@yahoo.com>
2017-11-22 12:40:13 +1300

    changed the runscript to get rid of coverage report

--- runscript.sh
+++ runscript.sh
@@ -4,10 +4,10 @@ echo "workflow testing"
 py.test --junitxml results.xml 
 cp -r ./results.xml /var/lib/jenkins/workspace/pytest_workflow/results.xml
 
-python -m coverage run 
-python -m coverage xml -o coverage.xml
+#python -m coverage
+#python -m coverage xml -o coverage.xml
 
-cp -r ./coverage.xml /var/lib/jenkins/workspace/pytest_workflow/coverage.xml
+#cp -r ./coverage.xml /var/lib/jenkins/workspace/pytest_workflow/coverage.xml
 
         
        
